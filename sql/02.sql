36524eddc059a85dc69bc31be614dc0ccb60d5b5
Aser-Abdelfatah <aseraser490@gmail.com>
2023-03-12 21:33:23 -0700

    midterm done

--- sql/02.sql
+++ sql/02.sql
@@ -2,3 +2,16 @@
  * Write a SQL query SELECT query that:
  * computes the country with the most customers in it. 
  */
+
+
+SELECT country
+FROM CUSTOMER
+LEFT JOIN ADDRESS
+USING (ADDRESS_ID)
+LEFT JOIN CITY
+USING (CITY_ID)
+LEFT JOIN COUNTRY
+USING (COUNTRY_ID)
+GROUP BY country
+ORDER BY COUNT(*) DESC
+LIMIT 1;
--- sql/04.sql
+++ sql/04.sql
@@ -5,3 +5,21 @@
  * Lists the title of all movies where at least 2 actors were also in 'AMERICAN CIRCUS'.
  * (You may choose to either include or exclude the movie 'AMERICAN CIRCUS' in the results.)
  */
+
+
+SELECT TITLE
+FROM FILM
+INNER JOIN FILM_ACTOR
+USING (FILM_ID)
+WHERE ACTOR_ID
+IN
+(
+    SELECT ACTOR_ID
+    FROM FILM
+    INNER JOIN FILM_ACTOR
+    USING (FILM_ID)
+    WHERE TITLE ILIKE 'AMERICAN CIRCUS'
+)
+GROUP BY TITLE
+HAVING COUNT(*) > 1
+ORDER BY TITLE;
